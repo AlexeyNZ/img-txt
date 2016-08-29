984fda0bc466c857da20c79c06ba63c9b7672c08
AlexeyNZ <alex0220853890@gmail.com>
2016-08-29 20:39:29 +1200

    URL

--- js/main_new.js
+++ js/main_new.js
@@ -1,12 +1,10 @@
 var instruction = $("#instruction")[0];
 var imgSelector = $("#my-file-selector")[0];
-var sendUrl = $("#sendURL");
+var sendUrl = $("#sendURL")[0];
 
 
-imgSelector.addEventListener("change", function () {
+sendUrl.addEventListener("click", function () {
     instruction.innerHTML = "Just a sec while we analyse your text...";
-    var imgFile = new FormData(imgSelector);
-    console.log(imgFile );
      var params = {
             // Request parameters
             "language": "unk",
@@ -16,12 +14,12 @@ imgSelector.addEventListener("change", function () {
             url: "https://api.projectoxford.ai/vision/v1.0/ocr?" + $.param(params),
             beforeSend: function(xhrObj){
                 // Request headers
-                xhrObj.setRequestHeader("Content-Type","multipart/form-data");
+                xhrObj.setRequestHeader("Content-Type","application/json");
                 xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","4f602b13d2794dbcb09cfd5a9a0f2148");
             },
             type: "POST",
             // Request body
-            data: imgFile,
+            data: {"url":"https://www.w3.org/WAI/images/easychecks/resize-wrap.png"},
             processData: false
         })
         .done(function(data) {
