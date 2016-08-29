0fba4eb5d02de527b000347f869170c26fe5bbe3
AlexeyNZ <alex0220853890@gmail.com>
2016-08-29 20:18:09 +1200

    test2

--- js/main_new.js
+++ js/main_new.js
@@ -1,19 +1,22 @@
 var instruction = $("#instruction")[0];
 var imgSelector = $("#my-file-selector")[0];
+var sendUrl = $("#sendURL");
+
 
 imgSelector.addEventListener("change", function () {
     instruction.innerHTML = "Just a sec while we analyse your text...";
     var imgFile = new FormData(imgSelector);
+    console.log(imgFile );
      var params = {
             // Request parameters
             "language": "unk",
-            "detectOrientation ": "true",
+            "detectOrientation": "true",
         };
         $.ajax({
             url: "https://api.projectoxford.ai/vision/v1.0/ocr?" + $.param(params),
             beforeSend: function(xhrObj){
                 // Request headers
-                xhrObj.setRequestHeader("Content-Type","application/octet-stream");
+                xhrObj.setRequestHeader("Content-Type","multipart/form-data");
                 xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","4f602b13d2794dbcb09cfd5a9a0f2148");
             },
             type: "POST",
@@ -28,6 +31,8 @@ imgSelector.addEventListener("change", function () {
             alert("error");
         });
 });
+
+
 
 
 // https://www.w3.org/WAI/images/easychecks/resize-wrap.png
